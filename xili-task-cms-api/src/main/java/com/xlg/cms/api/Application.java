4909c05487937ddbfb8020c804671293ed51f563
1367379258 <1367379258@qq.com>
2021-02-16 15:54:57 +0800

    init 2

---
+++ xili-task-cms-api/src/main/java/com/xlg/cms/api/Application.java
@@ -0,0 +1,15 @@
+package com.xlg.cms.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangqingwei <wangqingwei@kuaishou.com>
+ * Created on 2021-02-16
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
